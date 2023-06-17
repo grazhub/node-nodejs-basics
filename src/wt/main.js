b90e7db7f62a288be6924892d0192ed1d493d3c9
Grazhina Lebeckaya <grazhina96@mail.ru>
2023-06-17 20:52:42 +0200

    feat: add subtask solution worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { cpus } from 'os';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+
+const workerUrl = new URL('./worker.js', import.meta.url);
+const workerPath = fileURLToPath(workerUrl);
+const cpusNum = cpus().length;
+const startNum = 10;
+const statusResolved = 'resolved';
+const statusError = 'error';
+
 const performCalculations = async () => {
-    // Write your code here
+	const calcFibonacci = (workerData) => new Promise(resolve => {
+		const worker = new Worker(workerPath, { workerData });
+
+		worker.on('message', data => resolve({ status: statusResolved, data }));
+		worker.on('error', () => resolve({ status: statusError, data: null }));
+	});
+
+	const calcPromises = [];
+
+	for (let index = 0; index < cpusNum; index++) {
+		calcPromises.push(calcFibonacci(startNum + index));
+	}
+
+	const data = await Promise.all(calcPromises);
+	console.log(data);
 };
 
 await performCalculations();
