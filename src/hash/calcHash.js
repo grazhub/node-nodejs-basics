1935b33c429821ba114fcf6488c80703428d9a7f
Grazhina Lebeckaya <grazhina96@mail.ru>
2023-06-17 10:45:24 +0200

    feat: add subtask solution hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+import { fileURLToPath } from 'url';
+
+const fileURL = new URL('./files/fileToCalculateHashFor.txt', import.meta.url);
+const filePath = fileURLToPath(fileURL);
+
 const calculateHash = async () => {
-    // Write your code here 
+	const sha256Hash = createHash('sha256');
+	const fileContent = await readFile(filePath);
+
+	sha256Hash.update(fileContent);
+
+	const hashHex = sha256Hash.digest('hex');
+	console.log(hashHex);
 };
 
 await calculateHash();
