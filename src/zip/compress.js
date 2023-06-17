25d8e9d382859e2b21fec18d5c8dea74178d53c5
Grazhina Lebeckaya <grazhina96@mail.ru>
2023-06-17 19:09:11 +0200

    feat: add subtask solution zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
+const fileURL = new URL('./files/fileToCompress.txt', import.meta.url);
+const archiveFileURL = new URL('./files/archive.gz', import.meta.url);
+const filePath = fileURLToPath(fileURL);
+const archiveFilePath = fileURLToPath(archiveFileURL);
+
 const compress = async () => {
-    // Write your code here 
+    await pipeline(createReadStream(filePath), createGzip(), createWriteStream(archiveFilePath));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import { createGunzip } from 'zlib';
+
+const fileURL = new URL('./files/fileToCompress.txt', import.meta.url);
+const archiveFileURL = new URL('./files/archive.gz', import.meta.url);
+const filePath = fileURLToPath(fileURL);
+const archiveFilePath = fileURLToPath(archiveFileURL);
+
 const decompress = async () => {
-    // Write your code here 
+    await pipeline(createReadStream(archiveFilePath), createGunzip(), createWriteStream(filePath));
 };
 
 await decompress();
