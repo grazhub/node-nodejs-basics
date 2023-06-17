8c4266cdf8face1acec50fa7661e3f1a64a59feb
Grazhina Lebeckaya <grazhina96@mail.ru>
2023-06-17 21:22:20 +0200

    feat: add subtask solution child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+
+const scriptUrl = new URL('./files/script.js', import.meta.url);
+const scriptPath = fileURLToPath(scriptUrl);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(scriptPath, args, { silent: true });
+
+	process.stdin.pipe(childProcess.stdin);
+	childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['1', '2']);
