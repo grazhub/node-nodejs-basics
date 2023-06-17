441efd482f0f42f8e2fe20b90bbd7bf4595e3811
Grazhina Lebeckaya <grazhina96@mail.ru>
2023-06-17 12:17:42 +0200

    feat: add subtask solution streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+
+const fileURL = new URL('./files/fileToRead.txt', import.meta.url);
+const filePath = fileURLToPath(fileURL);
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = createReadStream(filePath);
+	await pipeline(readableStream, process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+
+const fileURL = new URL('./files/fileToWrite.txt', import.meta.url);
+const filePath = fileURLToPath(fileURL);
+
 const write = async () => {
-    // Write your code here 
+    const writableStream = createWriteStream(filePath, { flags : 'a' });
+	await pipeline(process.stdin, writableStream);
 };
 
 await write();
